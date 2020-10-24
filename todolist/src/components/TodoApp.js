4a8a2fdb0d472f24233081b4a9db7425c8adca62
VietHoang <dvhoang2106@gmail.com>
2020-10-24 17:48:46 +0700

    add addTodo

--- todolist/src/components/TodoApp.js
+++ todolist/src/components/TodoApp.js
@@ -1,60 +1,71 @@
 import React from "react";
-import Header from '../components/layout/Header';
-import Todos from './Todos';
+import Header from "../components/layout/Header";
+import Todos from "./Todos";
+import AddTodo from "./AddTodo";
+import uuid from "uuid";
 
-class TodoApp extends React.Component{
-    handleCheckboxchange = id => {
-        console.log("checked on check box id =" + id);
-        this.setState({ 
-            todo: this.state.todos.map(todo =>{
-                if(todo.id === id){
-                    todo.completed = !todo.completed;
-                }
-                return todo;
-            })
-        })
-    };
-    deleteTodo = id =>{
-        console.log("delete",id);
-        this.setState({
-            todos:[
-                /* ... toan tu spread operator : lay todo hien tai */
-                ...this.state.todos.filter(todo => {
-                    /* ham filter() tra ve 1 mang cac phan tu thoa man dk nao do */
-                    return todo.id !== id;
-                })
-            ]
-        })
-    }
-    state = { 
-        todos: [
-            {
-                id: 1, 
-                title: "Work home",
-                completed : true 
-            },
-            {
-                id: 2, 
-                title: "GO  home",
-                completed : true 
-            },
-            {
-                id: 3, 
-                title: "Do morning EX",
-                completed : false
-            }
-        ]
-    };
-    render(){
-        return (
-            <div className = "container">
-                <Header></Header>
-                <Todos todos={this.state.todos} 
-                handleChange = {this.handleCheckboxchange}
-                deleteTodo = {this.deleteTodo}
-                ></Todos>,
-            </div>
-        );
-    }
+class TodoApp extends React.Component {
+  handleCheckboxchange = (id) => {
+    this.setState({
+      todo: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
+  deleteTodo = (id) => {
+    this.setState({
+      todos: [
+        /* ... toan tu spread operator : lay todo hien tai */
+        ...this.state.todos.filter((todo) => {
+          /* ham filter() tra ve 1 mang cac phan tu thoa man dk nao do */
+          return todo.id !== id;
+        }),
+      ],
+    });
+  };
+  addTodo = (title) => {
+    const newTodo = { 
+        id: uuid.v4(), 
+        title: title, 
+        completed: false };
+    this.setState({ todos:
+        [...this.state.todos, newTodo] });
+  };
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Work home",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "GO  home",
+        completed: true,
+      },
+      {
+        id: 3,
+        title: "Do morning EX",
+        completed: false,
+      },
+    ],
+  };
+  render() {
+    return (
+      <div className="container">
+        <Header></Header>
+        <AddTodo addTodo={this.addTodo}></AddTodo>
+        <Todos
+          todos={this.state.todos}
+          handleChange={this.handleCheckboxchange}
+          deleteTodo={this.deleteTodo}
+        ></Todos>
+        
+      </div>
+    );
+  }
 }
 export default TodoApp;
